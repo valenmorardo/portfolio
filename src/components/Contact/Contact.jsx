787965d55f2eef5b7c7167bcfac4fdc7194730d8
Valentin <valentin.morardo@gmail.com>
2023-04-18 19:12:25 -0300

    contact finalizado, pagina finalizada, solo queda texto about me

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,55 @@
 import React from "react";
+import s from "./Contact.module.css";
+
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { GrMail } from "react-icons/gr";
+
+import externalLink from "../../assets/externalLink.png";
 
 const Contact = () => {
-  return <section>Contact xd
-  xdd</section>;
+  return (
+    <section className={s.section}>
+      <h1 className={s.title}>CONTACT</h1>
+
+      <div className={s.container}>
+        <a
+          href="https://www.linkedin.com/in/valentin-morardo/"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="Linkedin"
+          className={s.card}
+        >
+          <div>
+            <BsLinkedin className={s.icon} />
+            <h1>Linkedin</h1>
+            <span>linkedin.com/in/valentin-morardo</span>
+          </div>
+        </a>
+
+        <a
+          href="https://github.com/Littyfever"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="GitHub"
+          className={s.card}
+        >
+          <div>
+            <BsGithub className={s.icon} />
+            <h1>GitHub</h1>
+            <span>github.com/Littyfever</span>
+          </div>
+        </a>
+
+        <a href="mailto:valentin.morardo@gmail.com" aria-label="Email"  className={s.card}>
+          <div>
+            <GrMail className={s.icon} />
+            <h1>Mail</h1>
+            <span>valentin.morardo@gmail.com</span>
+          </div>
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
