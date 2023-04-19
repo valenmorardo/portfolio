7a4e5ea000cafe92711e9170725b95b8b054daa5
Valentin <valentin.morardo@gmail.com>
2023-04-18 22:38:34 -0300

    portfolio finalizado

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,28 +1,53 @@
 import React from "react";
 import s from "./AboutMe.module.css";
 
-import {images} from "../../utils";
+import { images } from "../../utils";
 
 const AboutMe = () => {
   return (
-    <section  className={s.container}>
+    <section className={s.section}>
+      <h1 className={s.title}>ABOUT_ME</h1>
 
-      <div className={s.img}>
-        <img src={images.typingBro} alt="Code Typing bro" />
-      </div>
+      <div className={s.container}>
+        <div className={s.divImg}>
+          <img
+            src={images.typingBro}
+            alt="Code Typing bro"
+            className={s.image}
+          />
+        </div>
 
-      <div className={s.content}>
-        <h1>About_Me</h1>
-
-        
-
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt,
-          itaque ab ullam eos exercitationem fugiat ea fugit ipsam nesciunt
-          minima placeat assumenda dolorum aliquam? Corporis odit at nisi nobis
-          vitae.
-        </p>
-        
+        <div className={s.divP}>
+          <p>
+            Hello! My name is Valentín Morardo and I am a full-stack web
+            developer. Since I was a child, I have always been fascinated by
+            technology, especially computers, so I decided to dedicate myself to
+            programming. I consider myself a passionate, optimistic, and
+            self-taught person, always facing challenges with a positive
+            attitude to overcome them.
+          </p>
+          <p className={s.parrafo}>
+            I have experience using{" "}
+            <a href="#techskills">technologies and tools</a> such as JavaScript,
+            ReactJS, Redux, HTML5, CSS3, NodeJS, ExpressJS, MongoDB, PostgreSQL,
+            and Git. Currently, I am dedicated to learning new content and
+            carrying out new projects to apply all my knowledge and continue
+            reinforcing it.
+          </p>
+          <p className={s.parrafo}>
+            Furthermore, I am attending an English academy here in my city to
+            improve and professionalize my English. During my free time, I enjoy
+            playing video games since I am an enthusiast and also do physical
+            training 2 or 3 times a week.
+          </p>
+          <p>
+            I am a responsible and committed person, and I am open to new job
+            opportunities and challenges. If you have any questions or if you
+            think we can collaborate on a project, don't hesitate to{" "}
+            <a href="#contact">contact me!</a> I am always willing to do my
+            best!
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 import s from "./Hero.module.css";
-import {images} from "../../utils";
-
+import { images } from "../../utils";
 
 const Home = () => {
-
-
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
   useEffect(() => {
@@ -23,67 +20,40 @@ const Home = () => {
     };
   }, []);
 
-
-
-  
   return (
-    <>
-    {!(screenWidth <= 1300) ? (
-         <section id="home" className={s.container}>
-         <div className={s.data}>
-           <span>Hello there! I&#39;m</span>
-           <h1>Valentin Morardo</h1>
-           <span>A full stack web developer</span>
-   
-           <h4>
-             "Working hard is important, but there is something that matters more,
-             believing in yourself"
-           </h4>
-   
-           <a href="/CV_ValentinMorardo.pdf" target="_blank" rel="noreferrer">
-             <button className={s.button}>GET_CURRICULUM</button>
-           </a>
-   
-           <a href="/#contact" >
-             <button className={s.button}>CONTACT_ME</button>
-           </a>
-         </div>
-   
-         <div className={s.img}>
-           <img src={images.profilePic} alt="img not found" />
-         </div>
-       </section>
-    ) :
+    <section id="home" className={s.section}>
+      <div className={s.container}>
+        <div className={s.content}>
+          <div className={s.data}>
+            <span>Hello there! I&#39;m</span>
+            <h1>Valentin Morardo</h1>
+            <span>A full stack web developer</span>
+          </div>
 
-    <section id="home" className={s.container}>
-      
-      <div className={s.img}>
-           <img src={images.profilePic} alt="img not found" />
+          <div className={s.frase}>
+            <h4>
+              "Working hard is important, but there is something that matters
+              more, believing in yourself"
+            </h4>
+          </div>
+
+          <div className={s.botones}>
+            <a href="/CV_ValentinMorardo.pdf" target="_blank" rel="noreferrer">
+              <button className={s.button}>GET_CURRICULUM</button>
+            </a>
+
+            <a href="/#contact">
+              <button className={s.button}>CONTACT_ME</button>
+            </a>
+          </div>
+        </div>
+
+        <div className={s.divImg}>
+          <img src={images.profilePic} alt="img not found"     className={s.image}/>
+        </div>
+
       </div>
-
-      <div className={s.data}>
-           <span>Hello there! I&#39;m</span>
-           <h1>Valentin Morardo</h1>
-           <span>A full stack web developer</span>
-   
-           <h4>
-             "Working hard is important, but there is something that matters more,
-             believing in yourself"
-           </h4>
-   
-           <a href="/CV_ValentinMorardo.pdf" target="_blank" rel="noreferrer">
-             <button className={s.button}>GET_CURRICULUM</button>
-           </a>
-   
-           <a href="/#contact" >
-             <button className={s.button}>CONTACT_ME</button>
-           </a>
-         </div>
-
     </section>
-    }
-    </>
-   
   );
 };
 
